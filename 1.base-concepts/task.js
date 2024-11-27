02d34ca8f55f6020b17b2de3a41d3c3f34cdc480
Kseny22 <misyura.c@yandex.ru>
2024-11-28 02:19:15 +0300

    Задания готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discr = Math.pow(b, 2) - 4 * a * c;
+  if (discr < 0) {
+  } else if (discr === 0) {
+    let solution = -b / (2 * a);
+    arr.push(solution);
+  } else if (discr > 0) {
+    let firstSolution = (-b + Math.sqrt(discr)) / (2 * a);
+    let secondSolution = (-b - Math.sqrt(discr)) / (2 * a);
+    arr.push(firstSolution, secondSolution);   }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+  percent = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+  totalPayment = Number(totalPayment.toFixed(2));
+  return totalPayment;
 }
+
